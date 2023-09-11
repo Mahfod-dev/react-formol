3d86cc0a2cb6a0ddb9ef4913c7dac15ec171ec98
Mahfod <mahfod.dev@gmail.com>
2023-09-11 23:49:47 +0200

    added config gh-pages

---
+++ vite.config.demo.js
@@ -0,0 +1,11 @@
+// vite.config.demo.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	build: {
+		outDir: 'demo_dist',
+	},
+});
